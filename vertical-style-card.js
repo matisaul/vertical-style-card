9bb9f443f7b7893d4c9cf0d3cabecedea68b184f
Matías Saul <34925604+matisaul@users.noreply.github.com>
2018-11-01 12:02:59 -0300

    Fixed problem with HA > 0.8.0

--- vertical-style-card.js
+++ vertical-style-card.js
@@ -22,6 +22,7 @@ class VerticalStyleCard extends HTMLElement {
 		if (!cardConfig.style.font_size || cardConfig.style.font_size == null ) cardConfig.style.font_size = 'var(--paper-font-headline_-_font-size)';
 
 		this.style.background = cardConfig.style.background_color;
+		this.style.padding = '5px 5px 5px 5px';
 
 		if (cardConfig.style.border == true){
 			this.style.boxShadow = "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.15)";
@@ -37,7 +38,7 @@ class VerticalStyleCard extends HTMLElement {
         if (config.title) {
             const title = document.createElement("div");
             title.className = "header";
-            title.style = "font-family: var(--paper-font-headline_-_font-family); -webkit-font-smoothing: var(--paper-font-headline_-_-webkit-font-smoothing); font-size: " + cardConfig.style.font_size + "; font-weight: var(--paper-font-headline_-_font-weight); letter-spacing: var(--paper-font-headline_-_letter-spacing); line-height: var(--paper-font-headline_-_line-height);text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);opacity: var(--dark-primary-opacity);padding: 10px 16px 10px 16px";
+            title.style = "font-family: var(--paper-font-headline_-_font-family); -webkit-font-smoothing: var(--paper-font-headline_-_-webkit-font-smoothing); font-size: " + cardConfig.style.font_size + "; font-weight: var(--paper-font-headline_-_font-weight); letter-spacing: var(--paper-font-headline_-_letter-spacing); line-height: var(--paper-font-headline_-_line-height);text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);opacity: var(--dark-primary-opacity);padding: 5px 16px 10px 16px";
             title.innerHTML = '<div class="name">' + config.title + '</div>';
             root.appendChild(title);
         }
@@ -64,23 +65,43 @@ class VerticalStyleCard extends HTMLElement {
 
     connectedCallback() {
         this._refCards.forEach((element) => {
-            if (element.shadowRoot) {
-                if (!element.shadowRoot.querySelector('ha-card')) {
-                    let searchEles = element.shadowRoot.getElementById("root");
-                    if (!searchEles) {
-                        searchEles = element.shadowRoot.getElementById("card");
-                    }
-                    searchEles = searchEles.childNodes;
-                    for(let i = 0; i < searchEles.length; i++) {
-                        searchEles[i].style.margin = "5px";
-                    }
-                } else {
-                    element.shadowRoot.querySelector('ha-card').style.boxShadow = 'none';
-                }
+            let fn = () => {
+                this._card(element);
+            };
+
+            if(element.updateComplete) {
+                element.updateComplete.then(fn);
+            } else {
+                fn();
             }
         });
     }
 
+    _card(element) {
+        if (element.shadowRoot) {
+            if (!element.shadowRoot.querySelector('ha-card')) {
+                let searchEles = element.shadowRoot.getElementById("root");
+                if (!searchEles) {
+                    searchEles = element.shadowRoot.getElementById("card");
+                }
+                if (!searchEles) return;
+                searchEles = searchEles.childNodes;
+                for (let i = 0; i < searchEles.length; i++) {
+                    searchEles[i].style.margin = "0px";
+                    this._card(searchEles[i]);
+                }
+            } else {
+                element.shadowRoot.querySelector('ha-card').style.boxShadow = 'none';
+            }
+        } else {
+            let searchEles = element.childNodes;
+            for (let i = 0; i < searchEles.length; i++) {
+                searchEles[i].style.margin = "0px";
+                this._card(searchEles[i]);
+            }
+        }
+    }
+    
     getCardSize() {
         let totalSize = 0;
         this._refCards.forEach((element) => {
